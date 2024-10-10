5fe3983df2cab1bf1abf4f8cb1a16ee41a8339d0
hmj6589 <hmj6589@gmail.com>
2024-10-10 11:24:37 +0900

    break-tests : n<0 삭제

--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@
 function factorial(n) { //5!=5
 
     
-    if( n < 0 ) {
-        throw new Error('n must be a positive integer');
-    }
+    // if( n < 0 ) {
+    //     throw new Error('n must be a positive integer');
+    // }
     
 
 
